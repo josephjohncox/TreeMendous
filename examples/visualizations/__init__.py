c007b282f4eafb1f4cead87e50e8be198676da7a
Joseph Cox <abovemind@gmail.com>
2025-09-30 16:55:15 -0700

    remove irrelavant docs

--- examples/visualizations/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Interactive visualizations and demos for Tree-Mendous interval trees.
-
-This package provides educational and analytical tools for understanding
-the behavior and performance characteristics of different interval tree
-implementations.
-"""
